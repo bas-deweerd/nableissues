1076b52b7a821bb99de9e8b24266a16d12054d1d
Bas de Weerd <bas-deweerd@live.nl>
2017-03-15 12:32:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         $api = new api();
         
         // This doesn't work:
-        $api->addUser('testmail@mail.com', 'pAs_w0rdd', '111', 'John', 'Lennon');
+        $api->addUser('testmail@mail.com', 'pAs_w0rdd', '111', 'John', 'Lennon'); // 111 is the ID of an existing customer
         
         // This does work:
         //$api->addCustomer("customername", "0622331122", "1122MM", "Kerkstraat 11", "", "Veldhoven", "NB", "NL", "Mr", "John", "Lennon", "0633221111", "31", "johnlennon@email.com", "Finance", "111");
