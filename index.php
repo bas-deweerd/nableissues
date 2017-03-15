55f4ec29db71c80dfbadf3abb80ebe7ec11fc19f
Bas de Weerd <bas-deweerd@live.nl>
2017-03-15 11:16:48 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        include 'api.php';
+        
+        $api = new api();
+        
+        // This doesn't work:
+        $api->addUser('testmail@mail.com', 'pAs_w0rdd', '111', 'John', 'Lennon');
+        
+        // This does work:
+        //$api->addCustomer("customername", "0622331122", "1122MM", "Kerkstraat 11", "", "Veldhoven", "NB", "NL", "Mr", "John", "Lennon", "0633221111", "31", "johnlennon@email.com", "Finance", "111");
+        
+        ?>
+    </body>
+</html>
